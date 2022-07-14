4cc3ef1b95fea33704de15d1a01eb6479bcf4a25
William Maitre <william.maitre@gmail.com>
2022-07-14 17:52:19 -0400

    finished exercise

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MethodsOverload__Exercise
+{
+    class Program
+    {
+      public static int Add(int num1, int num2)
+      {
+        return num1 + num2;
+       }
+      public static decimal Add(decimal num1, decimal num2)
+       {
+        return num1 + num2;
+        }
+      public static string Add(int num1, int num2, bool isCheck)
+        {
+            var sum = num1 + num2;
+
+            if(isCheck == true && sum >1)
+            {
+                return $"{sum} dollars";
+            }
+            else if(isCheck == true && sum ==1)
+            {
+                return $"{sum} dollar";
+            }
+            else if(isCheck == true && sum <1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return $"{num1 + num2}";
+            }
+        }
+        
+    }
+}
